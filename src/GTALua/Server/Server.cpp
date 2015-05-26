f1e32d43c5efc7f85b34ec13a3f3c75f7cb03a95
Michael Collins <duhprgrmman@gmail.com>
2015-05-25 17:24:45 -0700

    Created server ini

---
+++ src/GTALua/Server/Server.cpp
@@ -0,0 +1,28 @@
+#include "Server.h"
+
+// ctor
+Server::Server()
+{
+    
+}
+
+// dtor
+Server::~Server()
+{
+    
+}
+
+void Server::Init()
+{
+    
+}
+
+void Server::Close()
+{
+    
+}
+
+void Server::LoadServerIni()
+{
+    
+}
---
+++ src/GTALua/Server/Server.h
@@ -0,0 +1,61 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include "Mongoose/mongoose.h"
+#include <vector>
+#include <time.h>
+
+typedef char[48] ipAddr_t;
+
+// =================================================================================
+// Client descriptor
+// =================================================================================
+struct Client
+{
+    char* sName;
+    char[64] sSessionID;
+    ipAddr_t remoteAddress;
+    time_t tLastMessage;
+};
+
+// =================================================================================
+// Server configuration
+// =================================================================================
+struct ServerConfig
+{
+    int iPort;
+    bool bUsePassword;
+    char* sPassword;
+    bool bDefaultDeny;
+    std::vector<ipAddr_t> m_denyList;
+    std::vector<ipAddr_t> m_allowList;
+};
+
+// =================================================================================
+// Server
+// =================================================================================
+class Server
+{
+public:
+    Server();
+    ~Server();
+    
+    // Allow connections
+    void Init();
+    void Close();
+    
+    void LoadServerIni();
+    ServerConfig* GetConfig() {return &m_sConfig;}
+private:
+    bool m_bActive;
+    int m_ConnectedClients
+    ServerConfig m_sConfig;
+    mg_server *m_mgServer;
+};
+
+// =================================================================================
+// Global server instance
+// =================================================================================
+
+extern Server* g_pServer;
+#endif
