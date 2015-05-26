a3bdcd9b24c1519e7b79c5c769b12d5ae156ffd9
misomosi <duhprgrmman@gmail.com>
2015-05-26 13:17:10 -0700

    Fixed formatting on ServerINI.cpp for real this time. MSVC likes to hide that.

--- src/GTALua/Configuration/ServerINI.cpp
+++ src/GTALua/Configuration/ServerINI.cpp
@@ -11,38 +11,38 @@
 // =================================================================================
 int ini_server_parser(void* pCustom, const char* sSection, const char* sName, const char* sValue)
 {
-    // Server
+	// Server
 	if (_stricmp(sSection, "Server") == 0)
-    {
-        ServerConfig *config = (ServerConfig*) pCustom;
-        // Enabled
+	{
+		ServerConfig *config = (ServerConfig*)pCustom;
+		// Enabled
 		if (_stricmp(sName, "Enabled") == 0)
-        {
+		{
 			config->bEnabled = _stricmp(sValue, "true") == 0;
-        }
-        
-        // Port
+		}
+
+		// Port
 		if (_stricmp(sName, "Port") == 0)
-        {
-            config->iPort = atoi(sValue);
-        }
-        // Password
+		{
+			config->iPort = atoi(sValue);
+		}
+		// Password
 		if (_stricmp(sName, "PasswordProtected") == 0)
-        {
+		{
 			config->bUsePassword = _stricmp(sValue, "true") == 0;
-        }
+		}
 		if (_stricmp(sName, "Password") == 0)
-        {
-            config->sPassword = std::string(sValue);
-        }
-        
-        // IP whitelist
-        // (use access_control_list for mongoose)
+		{
+			config->sPassword = std::string(sValue);
+		}
+
+		// IP whitelist
+		// (use access_control_list for mongoose)
 		if (_stricmp(sName, "AccessControlList") == 0)
-        {
-            config->sACL = std::string(sValue);
-        }
-    }
+		{
+			config->sACL = std::string(sValue);
+		}
+	}
 	return 1;
 }
 
@@ -51,6 +51,6 @@ int ini_server_parser(void* pCustom, const char* sSection, const char* sName, co
 // =================================================================================
 void Server::LoadServerIni()
 {
-    *GetConfig() = {0}; // Shorthand for memset(0)
-    IniFile file("GTALua/gtalua.ini", ini_server_parser, GetConfig());
+	*GetConfig() = { 0 }; // Shorthand for memset(0)
+	IniFile file("GTALua/gtalua.ini", ini_server_parser, GetConfig());
 }
